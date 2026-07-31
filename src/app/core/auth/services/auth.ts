ec1afa6c4a1d7be78f668ebbaffefd3c8f9612b1
MaiVer4 <ll3925430@gmail.com>
2026-07-31 16:18:39 -0500

    feat(auth):agregar currentUser$ y saveSession en Auth service

--- src/app/core/auth/services/auth.ts
+++ src/app/core/auth/services/auth.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
-import { AuthResponse, LoginRequest, RegisterRequest } from '../models';
-import { Observable } from 'rxjs';
+import { AuthResponse, LoginRequest, RegisterRequest, User } from '../models';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -17,5 +17,13 @@ export class Auth {
 
   register(credentials: RegisterRequest): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.API_URL}/auth/register`, credentials)
+  }
+
+  private currentUser$ = new BehaviorSubject<User | null>(null)
+
+  private saveSession(response: AuthResponse): void {
+    localStorage.setItem('accessToken', response.accessToken)
+    localStorage.setItem('refreshToken', response.refreshToken)
+    this.currentUser$.next(response.user)
   }
 }
